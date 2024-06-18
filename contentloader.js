7e29ea71ecf633a9a7fd4bf213b8dd44907d293d
Nathaniel Berners <nathanieljb@pm.me>
2024-06-18 13:46:41 +0100

    Styling

--- contentloader.js
+++ contentloader.js
@@ -2,18 +2,18 @@
 document.addEventListener( 'DOMContentLoaded', () => {
     function    loadExternalHTML( url, targetElementID ) {
         fetch( url )
-            .then( response => {
-                if( !response.ok ) {
-                    throw new Error( `HTTP error! status: ${response.status}` );
-                }
-                return response.text();
-            })
-            .then( html => {
-                document.getElementById( targetElementID ).innerHTML = html;
-            })
-            .catch( error => {
-                console.error( 'Error loading external HTML:' error );
-            });
+        .then( response => {
+            if( !response.ok ) {
+                throw new Error( `HTTP error! status: ${response.status}` );
+            }
+            return response.text();
+        })
+        .then( html => {
+            document.getElementById( targetElementID ).innerHTML = html;
+        })
+        .catch( error => {
+            console.error( 'Error loading external HTML:' error );
+        });
     }
 
     loadExternalHTML( 'test.html', 'content' );
