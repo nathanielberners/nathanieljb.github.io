8ebc89b5481c3ad617a9a3f13d4abcbb27e2abef
Nathaniel Berners <nathanieljb@pm.me>
2024-06-18 12:31:08 +0100

    Styling

---
+++ contentloader.js
@@ -0,0 +1,20 @@
+// Content loader - Load content from external HTML files in to mian webpage
+document.addEventListener( 'DOMContentLoaded', () => {
+    function    loadExternalHTML( url, targetElementID ) {
+        fetch( url )
+            .then( response => {
+                if( !response.ok ) {
+                    throw new Error( 'HTTP error! status: ${response.status}' );
+                }
+                return response.text();
+            })
+            then( html => {
+                document.getElementById( targetElementID ).innerHTML = html;
+            })
+            .catch( error => {
+                console.error( 'Error loading external HTML:' error );
+            })
+    }
+
+    loadExternalHTML( 'test.html', 'content' );
+});
