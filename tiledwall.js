6c5a73aff9eebaa8f1cc9193644566b6e6683118
Nathaniel Berners <nathanieljb@pm.me>
2024-06-24 11:50:00 +0100

    Expanding TiledWall

--- tiledwall.js
+++ tiledwall.js
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const files = data.files;
         // Sort the files by date in descending order
         files.sort((a, b) => {
-          const dateA = new Date(a.match(/(\d{4}-\d{2}-\d{2})/)[0]);
-          const dateB = new Date(b.match(/(\d{4}-\d{2}-\d{2})/)[0]);
+          const dateA = new Date(a.match(/(\d{4}-\d{2}-\d{2})/)[0] + 'T00:00:00');
+          const dateB = new Date(b.match(/(\d{4}-\d{2}-\d{2})/)[0] + 'T00:00:00');
           return dateB - dateA; // Descending order
         });
         files.forEach(file => {
